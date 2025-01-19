d4e8ab6bbe973c5960301465ae6a3d18aada36eb
DOYAMOY CHAKMA <112064847+47dmc@users.noreply.github.com>
2025-01-20 00:11:58 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-<<<<<<< HEAD
-  base: '/flexbank-app/',
-=======
   base: '/flexbank-app',
->>>>>>> 5dd80190603b57bad635473ed867468dcd11a5e3
   plugins: [react()],
 })
